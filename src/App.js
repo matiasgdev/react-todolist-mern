ccf205edfe7530165f9307b3952d4ad1d5411b53
matiasgdev <matiasg.developer@gmail.com>
2020-11-04 18:25:21 -0300

    React Lazy for pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import React, {Suspense} from 'react'
 import Layout from './components/Layout'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import UserContextProvider from './context/UserContext'
 
-import HomePage from './pages/HomePage'
-import TaskDetailPage from './pages/TaskDetailPage'
-import ProfilePage from './pages/ProfilePage'
-import LoginPage from './pages/LoginPage'
-import NotFoundPage from './pages/404'
+// import HomePage from './pages/HomePage'
+// import TaskDetailPage from './pages/TaskDetailPage'
+// import ProfilePage from './pages/ProfilePage'
+// import LoginPage from './pages/LoginPage'
+// import NotFoundPage from './pages/404'
+
+
+const HomePage = React.lazy(() => import('./pages/HomePage'))
+const TaskDetailPage = React.lazy(() => import('./pages/TaskDetailPage'))
+const ProfilePage = React.lazy(() => import('./pages/ProfilePage'))
+const LoginPage = React.lazy(() => import('./pages/LoginPage'))
+const NotFoundPage = React.lazy(() => import('./pages/404'))
+
 
 function App() {
   return (
@@ -14,27 +23,29 @@ function App() {
       <Router>
         <Layout>
           <Switch>
-            <Route
-              exact path="/"
-              component={HomePage}
-            />
-            <Route
-              path="/perfil"
-              component={ProfilePage}
-            />
-            <Route
-              path="/identificarse"
-              component={LoginPage}
-            />
-            <Route
-              path="/tarea/:id"
-              component={TaskDetailPage}
-            />
-            {/* handle 404 route */}
-            <Route 
-              path="*" 
-              component={NotFoundPage}
-            />
+              <Suspense fallback={null} >
+                <Route
+                  exact path="/"
+                  component={HomePage}
+                />
+                <Route
+                  path="/perfil"
+                  component={ProfilePage}
+                />
+                <Route
+                  path="/identificarse"
+                  component={LoginPage}
+                />
+                <Route
+                  path="/tarea/:id"
+                  component={TaskDetailPage}
+                />
+                {/* handle 404 route */}
+                <Route 
+                  path="*" 
+                  component={NotFoundPage}
+                />
+              </Suspense>
           </Switch>
         </Layout>
       </Router>
